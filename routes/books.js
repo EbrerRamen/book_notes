50f998222f6a8ffbc9e833784016a64d1ccffa1e
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2026-06-18 19:25:39 +0600

    View book details

--- routes/books.js
+++ routes/books.js
@@ -54,6 +54,27 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+// Book details page
+router.get("/book/:id", async (req, res, next) => {
+  try {
+    const result = await db.query(
+      "SELECT * FROM books WHERE id = $1",
+      [req.params.id]
+    );
+
+    // If no book found
+    if (result.rows.length === 0) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.render("details.ejs", {
+      book: result.rows[0],
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Add book form
 router.get("/add", (req, res) => {
     res.render("add.ejs");
